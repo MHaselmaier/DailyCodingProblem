e85cec7c0a306ba36c44ca894165fb4af5bc267c
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2019-04-21 19:22:56 +0200

    Added a main-Methode in the first problem.

--- Problem #1/src/Problem.java
+++ Problem #1/src/Problem.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Problem
@@ -34,6 +35,29 @@ public class Problem
 	
 	public static void main(String[] args)
 	{
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("How many numbers would you like to enter?");
+		int[] numbers = new int[sc.nextInt()];
+				
+		for (int i = 0; numbers.length > i; ++i)
+		{
+			System.out.println("Please enter number #" + (i + 1) + ":");
+			numbers[i] = sc.nextInt();
+		}
+		
+		System.out.println("What is should the sum of two pairs be?");
+		int k = sc.nextInt();
+		
+		sc.close();
+		
+		if (containsPairEqualToKOnePass(numbers, k))
+		{
+			System.out.println("The entered numbers contain a pair which adds up to " + k + ".");
+		}
+		else
+		{
+			System.out.println("The entered numbers don't contain a pair which adds up to " + k + ".");
+		}
 	}
 }
