ca45e9a0c34efc9ad9bed22f25385fca9d88785f
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2019-04-24 22:57:35 +0200

    Solved problem #4.

---
+++ Problem #4/src/Problem.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Problem
+{
+	// Constraint: linear time and constant space
+	public static int findFirstMissingPositiveInteger(int[] input)
+	{
+		if (null == input) return 1;
+		
+		int newLength = input.length;
+		for (int i = 0; newLength > i; ++i)
+		{
+			if (0 >= input[i] || input.length < input[i])
+			{
+				int tmp = input[--newLength];
+				input[newLength] = input[i];
+				input[i] = tmp;
+			}
+		}
+		
+		for (int i = 0; newLength > i; ++i)
+		{
+			if (0 < input[Math.abs(input[i]) - 1])
+			{
+				input[Math.abs(input[i]) - 1] *= -1;
+			}
+		}
+		
+		for (int i = 0; newLength > i; ++i)
+		{
+			if (0 < input[i])
+				return i + 1;
+		}
+		
+		return newLength + 1;
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("How many numbers would you like to enter?");
+		int[] numbers = new int[sc.nextInt()];
+				
+		for (int i = 0; numbers.length > i; ++i)
+		{
+			System.out.println("Please enter number #" + (i + 1) + ":");
+			numbers[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		int missing = findFirstMissingPositiveInteger(numbers);
+		System.out.println("The first missing positive integer is " + missing + ".");
+	}
+}
---
+++ Problem #4/src/ProblemTest.java
@@ -0,0 +1,77 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProblemTest
+{
+	@Test
+	void testExampleFromProblem1()
+	{
+		int[] input = {3, 4, -1, 1};
+		assertEquals(2, Problem.findFirstMissingPositiveInteger(input));
+	}
+	
+	@Test
+	void testExampleFromProblem2()
+	{
+		int[] input = {1, 2, 0};
+		assertEquals(3, Problem.findFirstMissingPositiveInteger(input));
+	}
+	
+	@Test
+	void testNullArray()
+	{
+		assertEquals(1, Problem.findFirstMissingPositiveInteger(null));
+	}
+	
+	@Test
+	void testEmptyArray()
+	{
+		assertEquals(1, Problem.findFirstMissingPositiveInteger(new int[0]));
+	}
+	
+	@Test
+	void testStartingWithZero()
+	{
+		int[] input = {0, 3, 4, 2, 1};
+		assertEquals(5, Problem.findFirstMissingPositiveInteger(input));
+	}
+	
+	@Test
+	void testStartingWithNegative()
+	{
+		int[] input = {-4, 3, 4, 2, 1};
+		assertEquals(5, Problem.findFirstMissingPositiveInteger(input));
+	}
+	
+	@Test
+	void testLargeValues()
+	{
+		int[] input = {1_000_000, 999_999, 1, 2, 3};
+		assertEquals(4, Problem.findFirstMissingPositiveInteger(input));
+	}
+
+	@Test
+	void testRisingSequence()
+	{
+		assertEquals(11, Problem.findFirstMissingPositiveInteger(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+	}
+	
+	@Test
+	void testFallingSequnce()
+	{
+		assertEquals(11, Problem.findFirstMissingPositiveInteger(new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}));
+	}
+	
+	@Test
+	void testScrammbledStartingLow()
+	{
+		assertEquals(11, Problem.findFirstMissingPositiveInteger(new int[] {1, 10, 2, 9, 3, 8, 4, 7, 5, 6}));
+	}
+	
+	@Test
+	void testScrambledStartingHigh()
+	{
+		assertEquals(11, Problem.findFirstMissingPositiveInteger(new int[] {10, 1, 9, 2, 8, 3, 7, 4, 6, 5}));
+	}
+}
